1cafd55a2fa5ba9297e151d2cc65e4ce27eced85
glove0x0a-png <glove0x0a@gmail.com>
2025-09-17 17:11:40 +0900

    Update behavior_capslock_led.c

--- app/src/behaviors/behavior_capslock_led.c
+++ app/src/behaviors/behavior_capslock_led.c
@@ -8,7 +8,7 @@
 static const struct gpio_dt_spec caps_led = GPIO_DT_SPEC_GET(CAPS_LED_NODE, gpios);
 
 static void capslock_handler(const struct zmk_hid_indicators_changed *ev) {
-    gpio_pin_set_dt(&caps_led, ev->indicators.caps_lock);
+    gpio_pin_set_dt(&caps_led, ev->indicators.caps_lock? 0:1);
 }
 
 ZMK_EVENT_SUBSCRIBE(zmk_hid_indicators_changed, capslock_handler);
