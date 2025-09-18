0fcb1449600e262487ee43ec0738757565df3fdb
glove0x0a-png <glove0x0a@gmail.com>
2025-09-18 21:35:35 +0900

    Update behavior_capslock_led.c

--- app/src/behaviors/behavior_capslock_led.c
+++ app/src/behaviors/behavior_capslock_led.c
@@ -2,13 +2,15 @@
 #include <zephyr/drivers/gpio.h>
 #include <zmk/event_manager.h>
 #include <zmk/events/hid_indicators_changed.h>
+#include <zmk/hid.h>  // ← 追加：インジケータ取得用
 
 #define CAPS_LED_NODE DT_ALIAS(led_red)  // DeviceTreeで定義したLEDノード
 
 static const struct gpio_dt_spec caps_led = GPIO_DT_SPEC_GET(CAPS_LED_NODE, gpios);
 
 static void capslock_handler(const struct zmk_hid_indicators_changed *ev) {
-    gpio_pin_set_dt(&caps_led, ev->indicators.caps_lock? 0:1);
+    bool caps = zmk_hid_indicators() & HID_INDICATOR_CAPS_LOCK;
+    gpio_pin_set_dt(&caps_led, caps ? 0 : 1);
 }
 
 ZMK_EVENT_SUBSCRIBE(zmk_hid_indicators_changed, capslock_handler);
