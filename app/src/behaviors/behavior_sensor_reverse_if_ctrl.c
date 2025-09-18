789bcbed4404dcee2d06e643895d7bda4fc9a757
glove0x0a-png <glove0x0a@gmail.com>
2025-09-19 00:50:40 +0900

    Update behavior_sensor_reverse_if_ctrl.c

--- app/src/behaviors/behavior_sensor_reverse_if_ctrl.c
+++ app/src/behaviors/behavior_sensor_reverse_if_ctrl.c
@@ -1,13 +1,10 @@
-#include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zephyr/init.h>
-#include <dt-bindings/input/input-event-codes.h>
-#include <zmk/event-manager.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/kernel.h>
+
+#include <zmk/behavior.h>
 #include <zmk/events/sensor_event.h>
 #include <zmk/hid.h>
-#include <zmk/keymap.h>
-#include <zmk/endpoints.h>
-#include <zmk/behavior.h>
 #include <zmk/modifiers.h>
 
 struct sensor_rev_cfg {
@@ -17,7 +14,7 @@ struct sensor_rev_cfg {
 static int sensor_rev_if_ctrl_handler(const struct zmk_behavior_binding *binding,
                                       const struct sensor_event *event) {
     const struct device *dev = zmk_behavior_get_binding_device(binding);
-    if (dev == NULL) {
+    if (!dev) {
         return -ENODEV;
     }
 
@@ -25,11 +22,9 @@ static int sensor_rev_if_ctrl_handler(const struct zmk_behavior_binding *binding
     int direction = cfg->direction;
 
     bool ctrl_active = zmk_mods_active() & MOD_LCTRL;
-
     int final_direction = ctrl_active ? -direction : direction;
 
     zmk_hid_mouse_scroll(final_direction);
-
     return 0;
 }
 
@@ -38,9 +33,6 @@ static const struct behavior_driver_api sensor_rev_if_ctrl_driver_api = {
 };
 
 #define DT_DRV_COMPAT zmk_behavior_sensor_reverse_if_ctrl
-#include <zephyr/device.h>
-#include <zephyr/devicetree.h>
-#include <zephyr/sys/util.h>
 
 #define SENSOR_REV_INST(n)                                             \
     static struct sensor_rev_cfg sensor_rev_cfg_##n = {               \
