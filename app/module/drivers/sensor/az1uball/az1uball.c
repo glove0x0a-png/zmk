1b4cb93c4c54c0d3203fed8bc7a174aec30c3d36
glove0x0a-png <glove0x0a@gmail.com>
2025-10-13 10:55:54 +0900

    test

--- app/module/drivers/sensor/az1uball/az1uball.c
+++ app/module/drivers/sensor/az1uball/az1uball.c
@@ -118,12 +118,12 @@ void az1uball_read_data_work(struct k_work *work)
         } else if (delta_y != 0){  //レイヤーが3、かつ、y軸移動が <> 0
             if (delta_y > 10) {
                 // 下方向 → ボリュームダウン
-                input_report(data->dev, INPUT_KEY_VOLUMEDOWN, 1, true, 0); // 押下
-                input_report(data->dev, INPUT_KEY_VOLUMEDOWN, 0, true, 0); // 離す
+                input_report(data->dev, INPUT_KEY_VOLUMEDOWN, 1, true, 0,NULL); // 押下
+                input_report(data->dev, INPUT_KEY_VOLUMEDOWN, 0, true, 0,NULL); // 離す
             } else if (delta_y < -10) {
                 // 上方向 → ボリュームアップ
-                input_report(data->dev, INPUT_KEY_VOLUMEUP, 1, true, 0);   // 押下
-                input_report(data->dev, INPUT_KEY_VOLUMEUP, 0, true, 0);   // 離す
+                input_report(data->dev, INPUT_KEY_VOLUMEUP, 1, true, 0,NULL);   // 押下
+                input_report(data->dev, INPUT_KEY_VOLUMEUP, 0, true, 0,NULL);   // 離す
             }
         }
     }
