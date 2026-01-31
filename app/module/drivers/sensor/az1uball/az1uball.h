99a44fab698b4ffc9ddd7100102f796ee5be05cb
glove0x0a-png <glove0x0a@gmail.com>
2026-01-31 19:03:34 +0900

    test

---
+++ app/module/drivers/sensor/az1uball/az1uball.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/mutex.h>
+
+/* Bit Masks */
+#define MSK_SWITCH_STATE    0b10000000
+
+/* Mode definitions */
+enum az1uball_mode {
+    AZ1UBALL_MODE_MOUSE,
+    AZ1UBALL_MODE_SCROLL
+};
+
+struct az1uball_config {
+    struct i2c_dt_spec i2c;
+    const char *default_mode;
+    const char *sensitivity;
+};
+
+struct az1uball_data {
+    const struct device *dev;
+    struct k_work work;
+    struct k_timer polling_timer;
+    struct k_mutex data_lock;
+    bool sw_pressed;
+    bool sw_pressed_prev;
+    atomic_t x_buffer;
+    atomic_t y_buffer;
+    uint32_t last_interrupt_time;
+    uint32_t previous_interrupt_time;
+    int previous_x;
+    int previous_y;
+    int smoothed_x;
+    int smoothed_y;
+    uint32_t last_activity_time;    // 最後の入力があった時間
+    uint32_t last_jiggle_time;
+    bool is_connected;
+    bool is_active;
+};
